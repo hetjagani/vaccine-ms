500f75e94b7403f65553b0ee63fd318781655f1f
hetjagani <hetpatel572@gmail.com>
2021-11-22 17:52:18 -0800

    initialized project

---
+++ vms/src/main/java/com/cmpe275/vms/VmsApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe275.vms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsApplication.class, args);
+	}
+
+}
